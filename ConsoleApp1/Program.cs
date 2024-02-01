25f0358af898af21d1de3230a2b820e7275773a8
Alice Alessandrini <alice.alessandrini.12.12.1993@gmail.com>
2024-02-01 16:15:57 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            char scelta;
+
+            do
+            {
+                // MENU OPERAZIONI 
+                Console.WriteLine("================OPERAZIONI==============");
+                Console.WriteLine("Scegli l'operazione da effettuare:");
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Logout");
+                Console.WriteLine("3. Verifica ora e data di login");
+                Console.WriteLine("4. Lista degli accessi");
+                Console.WriteLine("5. Esci");
+                Console.WriteLine("========================================");
+                // FINE MENU
+
+                // Leggi l'input dell'utente
+                scelta = Console.ReadKey().KeyChar; // ottiene valore della key
+
+                switch (scelta)
+                {
+                    case '1':
+                        Utente.Login();
+                        break;
+                    case '2':
+                        Utente.Logout();
+                        break;
+                    case '3':
+                        Utente.StampaDataEOraLogin();
+                        break;
+                    case '4':
+                        Utente.StampaListaAccessi();
+                        break;
+                    case '5':
+                        Console.WriteLine("\nArrivederci!");
+                        break;
+                    default:
+                        Console.WriteLine("\nScelta non valida. Il programma si riavvierà.");
+                        break;
+                }
+
+            } while (scelta != '5'); // Continua fino a quando la scelta non è "Esci" (5)
+        }
+    }
+}
---
+++ ConsoleApp1/Utente.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal static class Utente
+    { // static => cioè non ha bisogno di avere più istanze
+        // n.b.: non si riferisce nè al tipo di dato nè all'essere pubblico/privato
+        public static string username;
+        private static string password;
+        private static bool loggedIn;
+        public static DateTime dataLogin; // gestito in login
+
+        //Metodo LOGIN
+        public static void Login() {
+            Console.WriteLine("\nInserisci username:");
+            Utente.username = Console.ReadLine();
+            Console.WriteLine("\nInserisci password:");
+            Utente.password = Console.ReadLine();
+            loggedIn = true; //
+            if (loggedIn == true)
+            {
+                dataLogin = DateTime.Now;
+               // StampaAccesso(dataLogin);
+            }
+        }
+
+        // METODO LOGOUT
+        public static void Logout()
+        {
+            if(loggedIn == true) 
+            {
+                Console.WriteLine("\nLogout effettuato con successo.");
+                loggedIn = false;
+                    } else {
+                Console.WriteLine("\nNessun utente loggato!");
+                    }
+        }
+        // METODO DATA E ORA
+        public static void StampaDataEOraLogin()
+        {
+            if (loggedIn == true)
+            {
+                Console.WriteLine($"\nUltimo accesso il: {dataLogin}");
+            }
+            else
+            { Console.WriteLine("\nNessun utente loggato"); }
+        }
+        // METODO LISTA ACCESSI
+        public static void StampaListaAccessi( ) 
+        { Console.WriteLine("\nLista accessi non disponibile"); }
+    }
+}
